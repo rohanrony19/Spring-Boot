f042e57acb55aeaa581d63d10b7c8edc91964940
Rohan <rohanprasad1434@gmail.com>
2025-11-01 23:30:28 +0530

    Dependency Injection

---
+++ SpringCore/src/main/java/com/springXmlConfigurationDI/constructor/injection/Product.java
@@ -0,0 +1,12 @@
+package com.springXmlConfigurationDI.constructor.injection;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+}
