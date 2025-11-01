b8c7c47af57faa714a6b56e09fb5811d0d33a070
Rohan <rohanprasad1434@gmail.com>
2025-11-01 23:31:50 +0530

    Dependency Injection

---
+++ SpringCore/src/main/java/com/springXmlConfigurationDI/SpringBeanAutoWiring/Patient.java
@@ -0,0 +1,12 @@
+package com.springXmlConfigurationDI.SpringBeanAutoWiring;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class Patient {
+    private int id;
+    private String name;
+}
