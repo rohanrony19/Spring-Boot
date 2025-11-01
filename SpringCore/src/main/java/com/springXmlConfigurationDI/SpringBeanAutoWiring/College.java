98a34113f7aeecb2e5f8ea4ad5515863a7bbb89f
Rohan <rohanprasad1434@gmail.com>
2025-11-01 23:31:29 +0530

    Dependency Injection

---
+++ SpringCore/src/main/java/com/springXmlConfigurationDI/SpringBeanAutoWiring/College.java
@@ -0,0 +1,11 @@
+package com.springXmlConfigurationDI.SpringBeanAutoWiring;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class College {
+    private String name;
+    private Student s1;
+}
