96d2ddf8691238e14492b9027be934b3229ed49d
Rohan <rohanprasad1434@gmail.com>
2025-11-02 23:09:56 +0530

    Dependency Injection

---
+++ SpringCore/src/main/java/com/springQualifier/PaymentConfig.java
@@ -0,0 +1,29 @@
+package com.springQualifier;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@ComponentScan("com.springQualifier")
+public class PaymentConfig {
+
+    @Primary
+    @Bean
+    public PaymentType creditCard(){
+        PaymentType payment = new PaymentType();
+        payment.setPaymentType("credit card");
+        return payment;
+    }
+
+    // default bean injection without using @Qualifier
+
+    @Bean
+    public PaymentType debitCard(){
+        PaymentType payment = new PaymentType();
+        payment.setPaymentType("debit card");
+        return payment;
+    }
+
+}
