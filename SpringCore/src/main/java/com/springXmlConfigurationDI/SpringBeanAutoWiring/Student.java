9ce98e824161bb9b529a271fcee4cf2b4e6dd55a
Rohan <rohanprasad1434@gmail.com>
2025-11-04 23:31:18 +0530

    spring

---
+++ SpringCore/src/main/java/com/springXmlConfigurationDI/SpringBeanAutoWiring/Student.java
@@ -0,0 +1,11 @@
+package com.springXmlConfigurationDI.SpringBeanAutoWiring;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+    private int id;
+    private String name;
+}
